caf59fe2d628bfcf65a9258598ccefccba98fcfd
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2023-10-26 22:08:42 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 
     plugins {
         kotlin("jvm") version "1.9.0"
-		kotlin("plugin.serialization") version "1.9.0"
+	kotlin("plugin.serialization") version "1.9.0"
         id("org.jetbrains.compose") version "1.5.0"
     }
 }
