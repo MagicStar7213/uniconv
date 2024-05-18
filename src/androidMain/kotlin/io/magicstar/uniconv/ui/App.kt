760b251f8e678ff2986194248730d0d9c5899e99
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2024-05-18 18:12:13 +0200

    Better distribution across screen

--- src/androidMain/kotlin/io/magicstar/uniconv/ui/App.kt
+++ src/androidMain/kotlin/io/magicstar/uniconv/ui/App.kt
@@ -42,172 +42,167 @@ fun App() {
     enabled = value != ""
     reference = updateMagnitudes(magnitudes, magnitude)
 
-    MaterialTheme {
-        Box(
-            modifier = Modifier.fillMaxSize()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(Res.drawable.uniconv),
+            contentDescription = "App logo",
+            modifier = Modifier.size(90.dp)
+        )
+        var magnMenuExpanded by remember { mutableStateOf(false) }
+
+        ExposedDropdownMenuBox(
+            modifier = Modifier.padding(
+                horizontal = 8.dp,
+                vertical = 8.dp
+            ),
+            expanded = magnMenuExpanded,
+            onExpandedChange = { magnMenuExpanded = it }
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+            OutlinedTextField(
+                modifier = Modifier.menuAnchor(),
+                shape = CircleShape,
+                value = magnitude,
+                onValueChange = {},
+                readOnly = true,
+                singleLine = true,
+                label = { Text(stringResource(Res.string.magnitudes)) },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(magnMenuExpanded) }
+            )
+            ExposedDropdownMenu(
+                expanded = magnMenuExpanded,
+                onDismissRequest = { magnMenuExpanded = false }
             ) {
-                Image(
-                    painter = painterResource(Res.drawable.uniconv),
-                    contentDescription = "App logo",
-                    modifier = Modifier.size(90.dp)
-                )
-                var magnMenuExpanded by remember { mutableStateOf(false) }
+                magnitudes.forEach {
+                    DropdownMenuItem(
+                        text = { Text(it) },
+                        onClick = {
+                            reference = updateMagnitudes(magnitudes, it)
+                            magnitude = it
 
-                ExposedDropdownMenuBox(
-                    modifier = Modifier.padding(
-                        horizontal = 8.dp,
-                        vertical = 15.dp
-                    ),
-                    expanded = magnMenuExpanded,
-                    onExpandedChange = { magnMenuExpanded = it }
-                ) {
-                    OutlinedTextField(
-                        modifier = Modifier.menuAnchor(),
-                        shape = CircleShape,
-                        value = magnitude,
-                        onValueChange = {},
-                        readOnly = true,
-                        singleLine = true,
-                        label = { Text(stringResource(Res.string.magnitudes)) },
-                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(magnMenuExpanded) }
-                    )
-                    ExposedDropdownMenu(
-                        expanded = magnMenuExpanded,
-                        onDismissRequest = { magnMenuExpanded = false }
-                    ) {
-                        magnitudes.forEach {
-                            DropdownMenuItem(
-                                text = { Text(it) },
-                                onClick = {
-                                    reference = updateMagnitudes(magnitudes, it)
-                                    magnitude = it
+                            unit1 = reference[unitIndex1]
+                            unit2 = reference[unitIndex2]
 
-                                    unit1 = reference[unitIndex1]
-                                    unit2 = reference[unitIndex2]
-
-                                    magnMenuExpanded = false
-                                }
-                            )
+                            magnMenuExpanded = false
                         }
-                    }
-                }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    OutlinedTextField(
-                        modifier = Modifier.padding(horizontal = 5.dp),
-                        singleLine = true,
-                        shape = CircleShape,
-                        label = { Text(stringResource(Res.string.value)) },
-                        value = value,
-                        onValueChange = {
-                            value = it
-                        }
-                    )
-
-                    var unit1MenuExpanded by remember { mutableStateOf(false) }
-
-                    ExposedDropdownMenuBox(
-                        modifier = Modifier.padding(
-                            horizontal = 8.dp,
-                            vertical = 15.dp
-                        ),
-                        expanded = unit1MenuExpanded,
-                        onExpandedChange = { unit1MenuExpanded = it }
-                    ) {
-                        OutlinedTextField(
-                            modifier = Modifier.menuAnchor(),
-                            shape = CircleShape,
-                            value = unit1.name,
-                            onValueChange = {},
-                            readOnly = true,
-                            singleLine = true,
-                            label = { Text(stringResource(Res.string.origin)) },
-                            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(unit1MenuExpanded) }
-                        )
-                        ExposedDropdownMenu(
-                            expanded = unit1MenuExpanded,
-                            onDismissRequest = { unit1MenuExpanded = false }
-                        ) {
-                            reference.forEach { unit ->
-                                DropdownMenuItem(
-                                    text = { Text(unit.name) },
-                                    onClick = {
-                                        unitIndex1 = reference.indexOf(unit)
-                                        unit1 = unit
-                                        unit1MenuExpanded = false
-                                    }
-                                )
-                            }
-                        }
-                    }
-
-                    Text(
-                        modifier = Modifier.padding(horizontal = 5.dp),
-                        text = stringResource(Res.string.to)
-                    )
-
-                    var unit2MenuExpanded by remember { mutableStateOf(false) }
-
-                    ExposedDropdownMenuBox(
-                        modifier = Modifier.padding(
-                            horizontal = 8.dp,
-                            vertical = 15.dp
-                        ),
-                        expanded = unit2MenuExpanded,
-                        onExpandedChange = { unit2MenuExpanded = it }
-                    ) {
-                        OutlinedTextField(
-                            modifier = Modifier.menuAnchor(),
-                            shape = CircleShape,
-                            value = unit2.name,
-                            onValueChange = {},
-                            readOnly = true,
-                            singleLine = true,
-                            label = { Text(stringResource(Res.string.target)) },
-                            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(unit2MenuExpanded) }
-                        )
-                        ExposedDropdownMenu(
-                            expanded = unit2MenuExpanded,
-                            onDismissRequest = { unit2MenuExpanded = false }
-                        ) {
-                            reference.forEach { unit ->
-                                DropdownMenuItem(
-                                    text = { Text(unit.name) },
-                                    onClick = {
-                                        unitIndex2 = reference.indexOf(unit)
-                                        unit2 = unit
-                                        unit2MenuExpanded = false
-                                    }
-                                )
-                            }
-                        }
-                    }
-                }
-                Button(
-                    modifier = Modifier.padding(vertical = 5.dp),
-                    enabled = enabled,
-                    onClick = {
-                        result ="${convert(value.toDouble(), unit1, unit2)} ${unit2.name}"
-                    }
-                ) {
-                    Text(
-                        text = stringResource(Res.string.convert),
-                        fontSize = 17.5.sp
-                    )
-                }
-                SelectionContainer {
-                    Text(
-                        text = result,
-                        fontSize = 25.sp
                     )
                 }
             }
         }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.weight(.4f),
+                singleLine = true,
+                shape = CircleShape,
+                label = { Text(stringResource(Res.string.value)) },
+                value = value,
+                onValueChange = {
+                    value = it
+                }
+            )
+
+            var unit1MenuExpanded by remember { mutableStateOf(false) }
+
+            ExposedDropdownMenuBox(
+                modifier = Modifier.padding(
+                    horizontal = 8.dp,
+                    vertical = 8.dp
+                ).weight(.3f),
+                expanded = unit1MenuExpanded,
+                onExpandedChange = { unit1MenuExpanded = it }
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier.menuAnchor(),
+                    shape = CircleShape,
+                    value = unit1.name,
+                    onValueChange = {},
+                    readOnly = true,
+                    singleLine = true,
+                    label = { Text(stringResource(Res.string.origin)) },
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(unit1MenuExpanded) }
+                )
+                ExposedDropdownMenu(
+                    expanded = unit1MenuExpanded,
+                    onDismissRequest = { unit1MenuExpanded = false }
+                ) {
+                    reference.forEach { unit ->
+                        DropdownMenuItem(
+                            text = { Text(unit.name) },
+                            onClick = {
+                                unitIndex1 = reference.indexOf(unit)
+                                unit1 = unit
+                                unit1MenuExpanded = false
+                            }
+                        )
+                    }
+                }
+            }
+
+            Text(
+                modifier = Modifier.padding(horizontal = 2.dp),
+                text = stringResource(Res.string.to)
+            )
+
+            var unit2MenuExpanded by remember { mutableStateOf(false) }
+
+            ExposedDropdownMenuBox(
+                modifier = Modifier.padding(
+                    horizontal = 8.dp,
+                    vertical = 8.dp
+                ).weight(.3f),
+                expanded = unit2MenuExpanded,
+                onExpandedChange = { unit2MenuExpanded = it }
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier.menuAnchor(),
+                    shape = CircleShape,
+                    value = unit2.name,
+                    onValueChange = {},
+                    readOnly = true,
+                    singleLine = true,
+                    label = { Text(stringResource(Res.string.target)) },
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(unit2MenuExpanded) }
+                )
+                ExposedDropdownMenu(
+                    expanded = unit2MenuExpanded,
+                    onDismissRequest = { unit2MenuExpanded = false }
+                ) {
+                    reference.forEach { unit ->
+                        DropdownMenuItem(
+                            text = { Text(unit.name) },
+                            onClick = {
+                                unitIndex2 = reference.indexOf(unit)
+                                unit2 = unit
+                                unit2MenuExpanded = false
+                            }
+                        )
+                    }
+                }
+            }
+        }
+        Button(
+            modifier = Modifier.padding(vertical = 5.dp),
+            enabled = enabled,
+            onClick = {
+                result ="${convert(value.toDouble(), unit1, unit2)} ${unit2.name}"
+            }
+        ) {
+            Text(
+                text = stringResource(Res.string.convert),
+                fontSize = 17.5.sp
+            )
+        }
+        SelectionContainer {
+            Text(
+                text = result,
+                fontSize = 25.sp
+            )
+        }
     }
+
 }
