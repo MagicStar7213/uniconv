0ab50ea470f00cdc3d7977627b48aad37ddb0894
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2024-05-25 13:40:26 +0200

    Moved line as overrode config

--- src/jvmMain/kotlin/io/magicstar/uniconv/data/Data.kt
+++ src/jvmMain/kotlin/io/magicstar/uniconv/data/Data.kt
@@ -18,8 +18,9 @@ fun initConfigFile() {
     if (!configFile.exists()) {
         configFile.parentFile.mkdirs()
         configFile.createNewFile()
+        configFile.writeText(json.encodeToString(Config(stringResource(Res.string.length), lengthUnits[0].name, lengthUnits[1].name)))
     }
-    configFile.writeText(json.encodeToString(Config(stringResource(Res.string.length), lengthUnits[0].name, lengthUnits[1].name)))
+
 }
 
 val json = Json { prettyPrint = true }
