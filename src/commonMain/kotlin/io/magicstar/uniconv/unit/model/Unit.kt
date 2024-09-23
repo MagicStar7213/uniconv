c2b1c3c5ed5b66e147fe859dc4db700e4d6ca193
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2024-09-22 22:45:35 -0500

    Fixed bug with conversion (or I think so, expect more commits otherwise)

--- src/commonMain/kotlin/io/magicstar/uniconv/unit/model/Unit.kt
+++ src/commonMain/kotlin/io/magicstar/uniconv/unit/model/Unit.kt
@@ -3,14 +3,18 @@ package io.magicstar.uniconv.unit.model
 interface Unit {
     val name: String
     val reference: Number
+
+abstract class SI<T>(override val name: String, override val reference: Number): Unit {
+    fun toImperial(value: Number): Double = T.toImperial(value)
     fun to(unit: T, value: Number): Double {
         return value.toDouble() * (reference.toDouble() / unit.reference.toDouble())
     }
 }
-
-abstract class SI<T>(override val name: String, override val reference: Number): Unit {
-    fun toImperial(value: Number): Double = T.toImperial(value)
 }
 abstract class Imperial<T>(override val name: String, override val reference: Number): Unit {
     fun toSI(value: Number): Double = T.toSI(value)
+    fun to(unit: T, value: Number): Double {
+        return value.toDouble() * (reference.toDouble() / unit.reference.toDouble())
+    }
+}
 }
