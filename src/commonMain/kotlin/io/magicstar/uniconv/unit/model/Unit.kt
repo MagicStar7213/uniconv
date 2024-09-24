52c3353a62ea3fc5c8665a88c9985831c17fb688
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2024-09-24 18:46:03 -0500

    Added val to unit property

--- src/commonMain/kotlin/io/magicstar/uniconv/unit/model/Unit.kt
+++ src/commonMain/kotlin/io/magicstar/uniconv/unit/model/Unit.kt
@@ -8,10 +8,10 @@ interface Unit {
     }
 }
 
-open class SI(override val name: String, override val reference: Number, unit: Unit): Unit {
+open class SI(override val name: String, override val reference: Number, val unit: Unit): Unit {
     fun toImperial(value: Number): Double = unit.toImperial(value) 
 }
 
-open class Imperial(override val name: String, override val reference: Number, unit: Unit): Unit {
+open class Imperial(override val name: String, override val reference: Number, val unit: Unit): Unit {
     fun toSI(value: Number): Double = unit.toSI(value)
 }
