90331f2069ceb79b718e1d6df34f1b5de4508df9
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2024-09-24 08:09:26 -0500

    Fixed problem with class nesting

--- src/commonMain/kotlin/io/magicstar/uniconv/unit/model/Unit.kt
+++ src/commonMain/kotlin/io/magicstar/uniconv/unit/model/Unit.kt
@@ -3,6 +3,7 @@ package io.magicstar.uniconv.unit.model
 interface Unit {
     val name: String
     val reference: Number
+}
 
 abstract class SI<T>(override val name: String, override val reference: Number): Unit {
     fun toImperial(value: Number): Double = T.toImperial(value)
@@ -10,11 +11,10 @@ abstract class SI<T>(override val name: String, override val reference: Number):
         return value.toDouble() * (reference.toDouble() / unit.reference.toDouble())
     }
 }
-}
+
 abstract class Imperial<T>(override val name: String, override val reference: Number): Unit {
     fun toSI(value: Number): Double = T.toSI(value)
     fun to(unit: T, value: Number): Double {
         return value.toDouble() * (reference.toDouble() / unit.reference.toDouble())
     }
-}
 }
