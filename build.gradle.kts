609e2c11b6a371f8d36a69d3fc86968cf867eed4
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2024-11-13 19:30:40 +0000

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
-    kotlin("multiplatform") version "2.0.10"
-    kotlin("plugin.serialization") version "2.0.10"
-    kotlin("plugin.compose") version "2.0.10"
-    id("org.jetbrains.compose") version "1.6.11"
+    kotlin("multiplatform") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("plugin.compose") version "2.0.21"
+    id("org.jetbrains.compose") version "1.7.0"
     id("com.android.application") version "8.5.2"
 }
 
